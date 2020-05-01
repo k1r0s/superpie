b7df7cbe9a92bb8bd712b622f4edd98435029087
k1r0s <ciroreed@gmail.com>
2020-05-01 14:08:24 +0200

    Revert "fix statefine based components not receiving props from outside"
    This reverts commit ee21b80d5e51de1c53533e0edc6cb47e8036c8dd.

--- demo/form/index.js
+++ demo/form/index.js
@@ -1,17 +1,10 @@
 import { render, statefine } from "superpie";
-import { Form } from "./form";
-import { Select } from "../select/select";
+import { Form, FormField } from "./form";
 
-const Main = statefine({ name: "", city: "" }, ({ state, setState }) => (
-  <Form from={state} onchange={setState}>
-    <div>
-      <label>name: </label>
-      <input data-field="name" />
-    </div>
-    <div>
-      <label>city: </label>
-      <Select data-field="city" options={["", "Alicante", "Valencia", "Castellón"]}/>
-    </div>
+const Main = statefine({ name: "", surname: "" }, ({ state, setState }) => (
+  <Form from={state} oninput={setState}>
+    <FormField label="Name" name="name"/>
+    <FormField label="Surname" name="surname"/>
     <pre>{JSON.stringify(state, null, 2)}</pre>
   </Form>
 ))
